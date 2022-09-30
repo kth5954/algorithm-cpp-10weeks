467ca94c9f94403da08c6fafe60cff834a9274e2
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-09-30 17:00:36 +0900

    초기 세팅

---
+++ cpp/2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void, char, string, bool, int, long long, double, unsigned long long
+
+
+int ret = 1;
+void a(){
+    ret = 2;
+    cout << ret << "\n";
+    return;
+}
+
+int main(){
+    a();
+    return 0;
+}
---
+++ cpp/4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double a(){
+    return 1.2333;
+}
+
+int main(){
+    string a = "wow";
+    a += " ";
+    a += "fantastic";
+    cout << a.size() << "\n";
+    cout << a << "\n";
+    return 0;
+}
+
