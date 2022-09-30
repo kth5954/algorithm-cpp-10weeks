9294d12deefc6d1f91f81dc5e6576c433daaba3f
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-09-30 17:09:10 +0900

    성능 최적화 템플릿

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -6,16 +6,7 @@ using namespace std;
 #define MOD 1000000000 
 #define MAX 202 #define COUNT 13 #define LOG 17
 
-double a(){
-    return 1.2333;
-}
-
 int main(){
-    string a = "wow";
-    a += " ";
-    a += "fantastic";
-    cout << a.size() << "\n";
-    cout << a << "\n";
-    return 0;
+
 }
 
