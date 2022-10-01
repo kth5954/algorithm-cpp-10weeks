5ad6cc35d0c13e54818efe8823876e0d999af8b7
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-10-01 18:49:08 +0900

    atoi

---
+++ cpp/7.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ) 
+#define BIGPRIME 48112959837082048697LL 
+#define INF 1000000000 
+#define MOD 1000000000 
+#define MAX 202 #define COUNT 13 #define LOG 17
+
+int main(){
+    string s = "1";
+    string s2 = "amumu";
+    cout << atoi(s.c_str()) << '\n';
+    cout << atoi(s2.c_str()) << '\n';
+    return 0;
+}
